d54757f69d3b67ce617877fc1571ac8b70c1d50b
kamran6433 <kamran_12@icloud.com>
2022-08-15 13:16:11 +0100

    Learning functions in Python.

---
+++ Functions.py
@@ -0,0 +1,11 @@
+# Functions are really important in programming because it cuts down large chunks of code into smaller chunks.
+# Functions are able to pass data through them and manipulate data through parameters.
+# Functions need to be called to be executed otherwise they will be redundant.
+def sum(x, y):
+    return x + y
+
+
+# Functions in Python need two lines of space after the last line because Python doesn't have curly braces to
+# structure code. Instead, Python uses indentation.
+print("Hello again world")
+print(sum(67, 90))
