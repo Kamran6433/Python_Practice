d8471c8e278794d2e7121d3f1832d38c625c233f
kamran6433 <kamran_12@icloud.com>
2022-08-15 08:58:21 +0100

    Learning lists in Python.

---
+++ Lists.py
@@ -0,0 +1,10 @@
+nums = [9, 87, 22, 213]
+print("nums[0]: " + nums[0])
+
+strings = ["Kamran", "Hello", "Attack On Titan"]
+print(nums[3])
+
+list_of_random_data_types = ["Car", 3.14, True, 1]
+print(list_of_random_data_types)
+list_of_lists = [nums, strings, list_of_random_data_types]
+print(list_of_lists)
