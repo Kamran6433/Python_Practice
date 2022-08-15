41e76a9eca1fac2615a4c29bbe241a42a0c0ffca
kamran6433 <kamran_12@icloud.com>
2022-08-15 08:23:43 +0100

    Added a HelloWorld file and added a message in it.

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+print("Hello World!\nThis is my first proper dive into Python.\n")
+print("I've been putting python off for a while now so im making this transition into Python.\n")
+print("I hope good things come from this.\n")
+print("I will learn Python intensively so I can start Machine Learning and building AI. 15/08/2022")
