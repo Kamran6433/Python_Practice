1536d354093c697d00e8d0ee07744e63ca921d71
kamran6433 <kamran_12@icloud.com>
2022-08-15 12:39:59 +0100

    Learning lists in Python.

--- Lists.py
+++ Lists.py
@@ -15,26 +15,45 @@ print(list_of_lists)
 # ----------------------------------------------------------------
 
 # This function sorts the list in ascending order (Java programmers crying right now including me).
+print("----------SORT-----------")
 nums.sort()
 print(nums)
 
 # This function does what it says. It reverses the entire list.
+print("----------REVERSE-----------")
 nums.reverse()
 print(nums)
 
 # This function works with the stack, popping (removing) the LAST element unless an index is passed as an argument.
+print("----------POP-----------")
 nums.pop()
 print(nums)
 
+# This function counts how many elements of the same value are in the list (There are 1 elements that have the value 9)
+print("----------COUNT-----------")
+print(nums.count(9))
+
 # This function makes a copy of nums and returns a new list.
+print("----------COPY-----------")
 print(nums.copy())
 
 # This function inserts a new value to a given index (index 1, in this example, is now 11010).
+print("----------INSERT-----------")
 nums.insert(1, 11010)
 print(nums)
 
 # This function allows the user to add multiple elements to a list.
-nums.extend([888, 777, 999])
+print("----------EXTEND-----------")
+nums.extend([8, 888, 777, 999, 8])
+print(nums)
+
+# This function returns the index of first element with specified value (Works well with duplicate elements in a list)
+print("----------INDEX-----------")
+print(nums.index(8))
+
+# This function adds to the end of the list
+print("----------APPEND-----------")
+nums.append(2222)
 print(nums)
 
 print(min(nums))
@@ -42,9 +61,11 @@ print(max(nums))
 print(sum(nums))
 
 # This function removes a given value in the list.
+print("----------REMOVE-----------")
 nums.remove(9)
 print(nums)
 
 # This function clears the entire list.
+print("----------CLEAR-----------")
 nums.clear()
 print(nums)
