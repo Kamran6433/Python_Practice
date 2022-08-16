ab51be25ba36afe9e82501ad597a38d5a7a7c511
kamran6433 <kamran_12@icloud.com>
2022-08-16 13:17:15 +0100

    Learning dictionaries in Python.

--- Dictionary.py
+++ Dictionary.py
@@ -14,3 +14,23 @@ print(dict.get(3))
 # To get the length of a dictionary you can use the len function.
 # **Notice how the length of the dictionary is referring to the pairs and NOT the keys and values individually.**
 print(len(dict))
+
+# You're able to mix and match different data types in dictionaries.
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    list: ["red", "white", "blue"],
+    1: 26.9877
+}
+print(thisdict)
+
+
+# There's a method called keys() that returns a list of all the keys in the dictionary.
+print(thisdict.keys())
+
+# Similarly there's a method called values() that returns a list of all the values in the dictionary.
+print(thisdict.values())
+
+# The items() method will return each item in a dictionary, as tuples in a list.
+print(thisdict.items())
