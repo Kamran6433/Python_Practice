733efc13e2b0935286a251263126a941f4af4225
kamran6433 <kamran_12@icloud.com>
2022-08-17 09:06:11 +0100

    Learning File reading.

--- ReadingFiles.py
+++ ReadingFiles.py
@@ -1,4 +1,14 @@
+# Reading files in Python is easy and effective.
+# User is able to read any file in a computer using its absolute path.
+# The open function is used to open a file by specifying the file name and the mode.
 
+# The modes are as follows:
+# "r" = read (default) | "r+" = read and write | "w" = write | "a" = append | "x" = create
+# "t" = Text mode (default) | "b" = Binary mode
+
+spotifyList = open("Spotify.txt", "r")
+print(spotifyList.read(1000))
+spotifyList.close()
 
 
 
