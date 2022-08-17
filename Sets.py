78c6e1480e5efc5e9ab555826d53b47529ee651f
kamran6433 <kamran_12@icloud.com>
2022-08-17 11:58:39 +0100

    Learning sets.

--- Sets.py
+++ Sets.py
@@ -2,7 +2,7 @@
 # A set is UNORDERED and UNINDEXED and DON'T ALLOW DUPLICATES.
 # Sets are written with curly braces:
 
-exampleSet = {1, 2, 8, 5, 1}
+exampleSet = {1, 2, 8, 5, 1, 54}
 print(exampleSet)  # When this set is printed, the order of the set is random
                    # and the duplicate entries are removed.
 
@@ -14,3 +14,21 @@ print("----------UNION-----------")
 # This method will Union (merge and remove duplicates) the two chosen sets together.
 unionSet = mixedSet.union(exampleSet)
 print(unionSet)
+
+print("----------DIFFERENCE-----------")
+# This method will find the Difference between the first set / second set.
+differenceSet = mixedSet.difference(exampleSet)
+print(differenceSet)
+
+print("----------INTERSECTION-----------")
+# This method will find the Difference between the first set / second set.
+intersectionSet = mixedSet.intersection(exampleSet)
+print(intersectionSet)
+
+print("----------UPDATE-----------")
+# This method will add items from another set into the set that the function is being called by.
+# The object in the update() method does not have to be a set.
+# It can be any iterable object (tuples, lists, dictionaries etc.).
+# WILL REMOVE DUPLICATES.
+mixedSet.update(exampleSet)
+print(mixedSet)
