38fb82ac01cc23b078f8b2ad022988d221c987a8
kamran6433 <kamran_12@icloud.com>
2022-08-17 15:51:23 +0100

    Learning OOP.

--- Classes&Objects.py
+++ Classes&Objects.py
@@ -1,3 +1,8 @@
+# Classes in Python are blueprints for object creation.
+# Classes can contain attributes or fields.
+# the __init__(self) function is a constructor for the class and is called automatically
+# to create an object of that class.
+# self refers to the object that is to be created and acts like "this." in Java.
 class Car:
     def __init__(self, name, model, year, real):
         self.name = name
@@ -11,6 +16,9 @@ class Car:
         else:
             return False
 
+    def print_everything(self):
+        return self.name, self.model, self.year, self.real
+
 
 class ProgrammingLanguage:
     def __init__(self, difficulty, oop, curly_braces, do_i_like):
@@ -22,11 +30,16 @@ class ProgrammingLanguage:
     def changed_my_mind(self, change):
         self.do_i_like = change
 
+    def print_everything(self):
+        return self.difficulty, self.oop, self.curly_braces, self.do_i_like
+
 
 car1 = Car("Chiron", "Bugatti", 2019, True)
 print(car1.name)
 print("Car modern? " + str(car1.is_modern()))
+print(car1.print_everything())
 
 python = ProgrammingLanguage("Easy", True, False, True)
 python.changed_my_mind(False)
 print("Do I like this language? " + str(python.do_i_like))
+print(python.print_everything())
