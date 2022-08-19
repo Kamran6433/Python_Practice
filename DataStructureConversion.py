6652034fa94dc8a49bad50486ed1ae24719841b5
kamran6433 <kamran_12@icloud.com>
2022-08-19 09:30:39 +0100

    Learning how to convert data structures to other data structures in Python..

--- DataStructureConversion.py
+++ DataStructureConversion.py
@@ -11,6 +11,16 @@ def list_to_set(random_list):
 
 
 a_list = [1, 0, 0, 1, 9, 8, 5, 5]
+another_list = ["my", 99, 99, 0, 0, 2.5, True]
+a_set = (1, 5, 7, "wth!")
+print(a_set[1])
 list_to_set(a_list)
 print("list to set conversion(1): " + str(my_set))
 
+new_set = set(another_list)
+print("Casting a list to a set: " + str(new_set))
+
+new_list = list(a_set)
+print("Casting a set to a list: " + str(new_list))
+
+# Casting a datastructure to another is more simple than I though.
