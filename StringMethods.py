fd72fa20ed1879cb09be8d045e3a9bad87ed2b12
kamran6433 <kamran_12@icloud.com>
2022-08-23 14:20:04 +0100

    Learning the string methods for Python.

--- StringMethods.py
+++ StringMethods.py
@@ -1,7 +1,40 @@
+# isalnum()	Returns True if all characters in the string are alphanumeric
+# isalpha()	Returns True if all characters in the string are in the alphabet
+# isascii()	Returns True if all characters in the string are ascii characters
+# isdecimal()	Returns True if all characters in the string are decimals
+# isdigit()	Returns True if all characters in the string are digits
+# isidentifier()	Returns True if the string is an identifier
+# islower()	Returns True if all characters in the string are lower case
+# isnumeric()	Returns True if all characters in the string are numeric
+# isprintable()	Returns True if all characters in the string are printable
+# isspace()	Returns True if all characters in the string are whitespaces
+# istitle()	Returns True if the string follows the rules of a title
+# isupper()	Returns True if all characters in the string are upper case
+# join()	Converts the elements of an iterable into a string
+# ljust()	Returns a left justified version of the string
+# lower()	Converts a string into lower case
+# lstrip()	Returns a left trim version of the string
+# maketrans()	Returns a translation table to be used in translations
+# partition()	Returns a tuple where the string is parted into three parts
+# replace()	Returns a string where a specified value is replaced with a specified value
+# rfind()	Searches the string for a specified value and returns the last position of where it was found
+# rindex()	Searches the string for a specified value and returns the last position of where it was found
+# rjust()	Returns a right justified version of the string
+# rpartition()	Returns a tuple where the string is parted into three parts
+# rsplit()	Splits the string at the specified separator, and returns a list
+# rstrip()	Returns a right trim version of the string
+# split()	Splits the string at the specified separator, and returns a list
+# splitlines()	Splits the string at line breaks and returns a list
+# strip()	Returns a trimmed version of the string
+# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+# title()	Converts the first character of each word to upper case
+# translate()	Returns a translated string
+# upper()	Converts a string into upper case
+# zfill()	Fills the string with a specified number of 0 values at the beginning
 # This file contains all teh string methods in Python. If I want to become a good programmer I must learn all the
 # string methods for each language as they are all important to mastering the language.
 
-text = "my name is Kamran and I will become a söftware engineer."
+text = "my name is Kamran and I will become a Söftware Engineer."
 
 capitalize = "CAPITALIZE"
 print(capitalize.center(60, "-"))
@@ -21,7 +54,7 @@ center = "CENTER"
 print(center.center(60, "-"))
 c = text.center(100, "*")
 print("Text: " + text)
-print("center(): " + c)
+print("center(100, \"*\"): " + c)
 # This method returns a string which is filled with the number of spaces specified with the string in the middle of
 # the space. The second argument is what should be filled in for the space. Default is " ".
 
@@ -29,7 +62,7 @@ count = "COUNT"
 print(count.center(60, "-"))
 d = text.count("a", 2, 5)
 print("Text: " + text)
-print("count(): " + str(d))
+print("count(\"a\", 2, 5): " + str(d))
 # This method returns the number of times a specified value appears in the string.
 # The second argument is the position to start the search and the third argument is the position to end the search.
 
@@ -49,20 +82,29 @@ print("Text: " + text)
 print(e)
 # This method encodes the string, using the specified encoding (default: UTF-8).
 
-endswith = "ENDSWITH"
+endswith = "ENDSWITH()"
 print(endswith.center(60, "-"))
 f = text.endswith(".")
 print("Text: " + text)
-print("count(): " + str(f))
-# This method returns true if the string ends with the specified value, otherwise false.
+print("endswith(\".\"): " + str(f))
+# This method returns true if the string starts with the specified value, otherwise false.
 # The second argument is the position to start the search and the third argument is the position to end the search.
 
-find = "FIND"
+startswith = "STARTSWITH()"
+print(startswith.center(60, "-"))
+f = text.startswith("my")
+print("Text: " + text)
+print("startswith(\"my\"): " + str(f))
+# This method returns true if the string starts with the specified value, otherwise false.
+# The second argument is the position to start the search and the third argument is the position to end the search.
+
+find = "FIND()"
 print(find.center(60, "-"))
 g = text.find("0", 10, 20)
 print("Text: " + text)
-print("count(): " + str(g))
+print("find(\"0\", 10, 20): " + str(g))
 # This method finds the first occurrence of the specified value. The method returns -1 if the value is
 # not found. The method is almost the same as the index() method, the only difference is that the index() method
 # raises an exception if the value is not found.
 # The second argument is the position to start the search and the third argument is the position to end the search.
+
