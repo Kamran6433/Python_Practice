38f0b6cb7ae062d3822b58c55f055907a30e9872
kamran6433 <kamran_12@icloud.com>
2023-02-20 21:46:53 +0000

    Answering Questions in Python.

---
+++ AnsweredQuestions/WhichAreIn?.py
@@ -0,0 +1,44 @@
+# Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+# Example 1:
+# a1 = ["arp", "live", "strong"]
+
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+# returns ["arp", "live", "strong"]
+
+# Example 2:
+# a1 = ["tarp", "mice", "bull"]
+
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+# returns []
+
+# Notes:
+# Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+# In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+# Beware: In some languages r must be without duplicates.
+
+# My Answer:
+def in_array(array1, array2):
+    answerString = []
+    for word in array2:
+        for word2 in array1:
+            if word2 in word:
+                answerString.append(word2)
+                
+    answerString = set(answerString)
+    answerString = list(answerString)
+    answerString.sort()
+    return answerString
+
+# Better Answer by tuxanek:
+def in_array(array1, array2):
+    # your code
+    res = []
+    for a1 in array1:
+        for a2 in array2:
+            if a1 in a2 and not a1 in res:
+                res.append(a1)
+    res.sort()
+    return res
